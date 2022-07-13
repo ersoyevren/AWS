6c0f429506231c6ce7b9b8b23ae8fafcd0d547ce
evren <ersoyevren34@gmail.com>
2022-07-13 23:11:07 +0200

    AWS CLI

---
+++ CLI/CLI.sh
@@ -0,0 +1,31 @@
+# AWS CLI
+
+# References
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-welcome.html
+# https://awscli.amazonaws.com/v2/documentation/api/latest/index.html
+# https://aws.amazon.com/blogs/compute/query-for-the-latest-amazon-linux-ami-ids-using-aws-systems-manager-parameter-store/
+
+
+
+# Installation
+
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+
+# Win:
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+
+# Mac:
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+# https://graspingtech.com/install-and-configure-aws-cli/
+
+
+# Linux:
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip  #install "unzip" if not installed
+sudo ./aws/install
+
+
